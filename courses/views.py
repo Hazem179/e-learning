3854383278e0e79a0eeddf57c832b3c1fedecb6a
Hazem <hazemm913@gmail.com>
2022-06-01 02:11:21 +0200

    formset

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,11 @@
 from django.urls import reverse_lazy
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .models import Course
+from .forms import ModuleFormSet
 
 
 # Create your views here.
@@ -20,7 +23,7 @@ class OwnerEditMixin(object):
         return super().form_valid(form)
 
 
-class OwnerCourseMixin(OwnerMixin,LoginRequiredMixin,PermissionRequiredMixin):
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
     model = Course
     fields = ['subject', 'title', 'slug', 'overview']
     success_url = reverse_lazy('manage_course_list')
@@ -69,3 +72,41 @@ to confirm the course deletion.
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'manage/course/delete.html'
     permission_required = 'courses.delete_course'
+
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'manage/module/formset.html'
+    course = None
+
+    """You define this method to avoid repeating the code to build 
+    the formset. You create a ModuleFormSet object for the given Course object 
+    with optional data"""
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course, data=data)
+
+
+    """: This method is provided by the View class. It takes an HTTP 
+request and its parameters and attempts to delegate to a lowercase method 
+that matches the HTTP method used. A GET request is delegated to the get()
+method and a POST request to post(), respectively. In this method, you use 
+the get_object_or_404() shortcut function to get the Course object for the 
+given id parameter that belongs to the current user. You include this code in 
+the dispatch() method because you need to retrieve the course for both GET
+and POST requests. You save it into the course attribute of the view to make 
+it accessible to other methods."""
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course, id=pk, owner=request.User)
+        return super().dispatch(request, pk)
+
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
